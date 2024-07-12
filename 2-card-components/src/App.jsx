bbee4f9e523320ffdb26e0e332cd43c91a39b4a3
Jiangjie Chen <franciscochen.cjj@gmail.com>
2024-07-12 16:15:59 -0300

    5-conditional rendering

--- 2-card-components/src/App.jsx
+++ 2-card-components/src/App.jsx
@@ -1,6 +1,7 @@
 // import Card from "./Card"
 // import Button from "./Button/Button"
-import Student from "./Student";
+// import Student from "./Student";
+import UserGreeting from "./UserGreeting";
 function App() {
   return(
     // fragmente:<></>
@@ -11,9 +12,12 @@ function App() {
     // <Button/>
     
     // 3-props
-      <>
-        <Student name ="Spongebob" age={30} isStudent={true}/>
-      </>
+      // <>
+      //   <Student name ="Spongebob" age={30} isStudent={true}/>
+      // </>
+    <>
+      <UserGreeting isLoggedIn={true} username="Fran"/>
+    </>
   );
 }
 
